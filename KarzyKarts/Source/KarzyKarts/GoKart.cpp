910249ec4f0e45ccd47fcffc0f98e81bf6a4c5be
Juan Couso <Cucsijuan@gmail.com>
2019-11-25 23:23:45 -0300

    S04E78 - Started Project, added new car pawn, made forward movement with acceleration, made collision detection

---
+++ KarzyKarts/Source/KarzyKarts/GoKart.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GoKart.h"
+#include "Components/InputComponent.h"
+
+// Sets default values
+AGoKart::AGoKart()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGoKart::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGoKart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector Force = MaxDrivingForce * Throttle * GetActorForwardVector();
+	
+	FVector Acceleration = Force / Mass;
+
+	Velocity += Acceleration * DeltaTime;
+
+	UpdateLocationFromVelocity(DeltaTime);
+
+}
+
+void AGoKart::UpdateLocationFromVelocity(float DeltaTime)
+{
+	FVector Translation = Velocity * 100 * DeltaTime;
+
+	FHitResult  Hit;
+
+	AddActorLocalOffset(Translation, true, &Hit);
+
+	if (Hit.IsValidBlockingHit())
+	{
+		Velocity = FVector::ZeroVector;
+	}
+}
+
+// Called to bind functionality to input
+void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+}
+
+void AGoKart::MoveForward(float Value)
+{
+	Throttle = Value;
+}
+
---
+++ KarzyKarts/Source/KarzyKarts/KarzyKartsHud.h
@@ -0,0 +1,22 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/HUD.h"
+#include "KarzyKartsHud.generated.h"
+
+
+UCLASS(config = Game)
+class AKarzyKartsHud : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	AKarzyKartsHud();
+
+	/** Font used to render the vehicle info */
+	UPROPERTY()
+	UFont* HUDFont;
+
+	// Begin AHUD interface
+	virtual void DrawHUD() override;
+	// End AHUD interface
+};
