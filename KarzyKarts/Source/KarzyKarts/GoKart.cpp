6a4fab9e838cccf1feb26d07c6df2e2b10e0b962
Juan Couso <Cucsijuan@gmail.com>
2019-12-29 01:52:22 -0300

    S04E97 - Fixed Simulated Proxy prediction and Auth + Simulated proxy bug

--- KarzyKarts/Source/KarzyKarts/GoKart.cpp
+++ KarzyKarts/Source/KarzyKarts/GoKart.cpp
@@ -74,19 +74,24 @@ void AGoKart::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (IsLocallyControlled())
+	if (Role == ROLE_AutonomousProxy)
 	{
 		FGoKartMove Move = CreateMove(DeltaTime);
-
-		if (!HasAuthority())
-		{
-			UnacknowloegedMoves.Add(Move);
-
-			UE_LOG(LogTemp, Warning, TEXT("Queque length: %d"), UnacknowloegedMoves.Num());
-		}
-
+		UnacknowloegedMoves.Add(Move);
 		SimulateMove(Move);
 		Server_SendMove(Move);
+	}
+
+	//We are the server and in control of the pawn
+	if (Role== ROLE_Authority && GetRemoteRole() == ROLE_SimulatedProxy)
+	{
+		FGoKartMove Move = CreateMove(DeltaTime);
+		Server_SendMove(Move);
+	}
+
+	if (Role == ROLE_SimulatedProxy)
+	{
+		SimulateMove(ServerState.LastMove);
 	}
 
 	DrawDebugString(GetWorld(), FVector(0, 0, 100), GetEnumText(Role), this, FColor::White, DeltaTime);
